bc03e7adc46b1cd6abdd70ef870ef217925c406d
Jason Hissong <jasonhissong2013@gmail.com>
2019-02-13 21:22:42 -0500

    adding Social Media Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ContactForm from './ContactForm';
 import CareerOpportunities from './CareerOpportunities';
+import SocialMedia from './SocialMedia';
 import './Footer.scss';
 
 class Footer extends Component {
@@ -12,10 +13,16 @@ class Footer extends Component {
           <div className="width-50">
             <ContactForm />
           </div>
-          <div className="width-50">
+          <div className="width-50 ">
             <CareerOpportunities />
+
           </div>
           <div className="clear"></div>
+          <div className="float-right">
+            <SocialMedia />
+          </div>
+          <div className="clear"></div>
+
 
         </div>
       </div>
---
+++ src/components/SocialMedia.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './SocialMedia.scss';
+
+class SocialMedia extends Component {
+
+  render() {
+    return (
+      <div className="social-media">
+        <a href="https://www.facebook.com/pages/J-G-Demolition/164127576947766" target="_blank">
+          <img src="images/social-media/fb-icon-white-cropped.png" alt="Facebook Page"/>
+        </a>
+        <a href="https://www.linkedin.com/in/juanita-weeks-92a73111" target="_blank">
+          <img src="images/social-media/linkedin-white-cropped.png" alt="linkedIn Page"/>
+        </a>
+
+
+      </div>
+    );
+  }
+
+}
+
+export default SocialMedia;
