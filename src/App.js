10089bdceffc5211cd4e61b2446a9b8f119a78a9
Jason Hissong <jasonhissong2013@gmail.com>
2019-03-28 21:00:55 -0400

    Certificate mobile responsive

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TopCertification from './components/TopCertification.js';
 import Navigation from './components/Navigation.js';
 import HeroImage from './components/HeroImage.js';
 import ServiceSection from './components/ServiceSection';
-//import CertificateSection from './components/CertificateSection';
+import CertificateSection from './components/CertificateSection';
 //import AboutUs from './components/AboutUs';
 //import Footer from './components/Footer';
 import './App.scss';
@@ -19,8 +19,8 @@ const App = () =>{
       <Navigation />
          <HeroImage />
         <ServiceSection />
-        {/*<CertificateSection />
-        <AboutUs />
+        <CertificateSection />
+        {/*<AboutUs />
         <Footer />*/}
       </div>
   );
