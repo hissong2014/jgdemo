a449c02c7fa98c58f9ad22f720ee70601b891163
Jason Hissong <jasonhissong2013@gmail.com>
2019-08-11 17:19:05 -0400

    Added Smooth Scrolling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './Navigation.scss';
 import Hamburger from './Hamburger.js';
+import * as Scroll from 'react-scroll';
+import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 
 class Navigation extends Component {
 
@@ -9,17 +11,20 @@ class Navigation extends Component {
 
       this.state = {
           navigation : [
-            {key:1, navTitle:"Services", navURL:"#services"},
-            {key:2, navTitle:"certifications", navURL:"#certifications"},
-            {key:3, navTitle:"About Us", navURL:"#about-us"},
-            {key:4, navTitle:"Contact Us", navURL:"#contact-us"},
+            {key:1, navTitle:"Services", navURL:"services"},
+            {key:2, navTitle:"certifications", navURL:"certifications"},
+            {key:3, navTitle:"About Us", navURL:"about-us"},
+            {key:4, navTitle:"Contact Us", navURL:"contact-us"},
           ],
           scrolled:false,
           isMenuActive:false
         }
   }
-
+  scrollToTop() {
+    scroll.scrollToTop();
+  }
   componentDidMount(){
+
     window.addEventListener('scroll', () =>{
       const isTop = window.scrollY < 103;
       if(isTop !== true){
@@ -44,13 +49,14 @@ class Navigation extends Component {
       <div className={this.state.scrolled ? 'navigation-container scrolled' : 'navigation-container'}>
 
         <div className="main-container">
-
-          <img
-            src="images/logo/jg-logo-website.png"
-            height="110"
-            alt="J&G Demolition"
-            className={this.state.scrolled ? 'logo-min logo' : 'logo'}
-          />
+          <a onClick={this.scrollToTop}>
+            <img
+              src="images/logo/jg-logo-website.png"
+              height="110"
+              alt="J&G Demolition"
+              className={this.state.scrolled ? 'logo-min logo' : 'logo'}
+            />
+          </a>
           <div className="show-hamburger" onClick={this.menuClicked.bind(this)}>
             <Hamburger/>
           </div>
@@ -59,7 +65,10 @@ class Navigation extends Component {
             {
               this.state.navigation.map(
                 navigation =>
-                <li key={navigation.key}><a href={navigation.navURL}>{navigation.navTitle}</a></li>
+                <li key={navigation.key}><Link
+                duration={500}
+                smooth={true}
+                className="menu-link" to={navigation.navURL}>{navigation.navTitle}</Link></li>
               )
             }
           </ul>
